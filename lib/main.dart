79362b4f5d390466f7ae01f611e944d61d3abf90
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-02-19 22:21:58 +0530

    redTheme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_supabase/screens/HomeScreen.dart';
-// import 'package:flutter_supabase/screens/LoginScreen.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
+import 'package:flutter_supabase/services/UiProvider.dart';
+import 'package:flutter_supabase/screens/HomeScreen.dart';
 import 'package:flutter_supabase/services/UiProvider.dart';
 import 'package:provider/provider.dart';
-
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: ".env");
@@ -18,10 +18,13 @@ void main() async {
     anonKey: dotenv.env['SUPA_BASE_ANON_KEY'] ?? "",
   );
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -29,18 +32,22 @@ class MyApp extends StatelessWidget {
       child: Consumer<UiProvider>(
         builder: (context, UiProvider notifier, child) {
           return MaterialApp(
+
             debugShowCheckedModeBanner: false,
 
             //By default theme setting, you can also set system
             // when your mobile theme is dark the app also become dark
             themeMode: notifier.isDark ? ThemeMode.dark : ThemeMode.light,
+
             //Our custom theme applied
             darkTheme: notifier.isDark ? notifier.darkTheme : notifier.lightTheme,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
+
+            theme: notifier.isDark
+                ? notifier.darkTheme // dark theme applied
+                : notifier.redTheme, // pink theme applied when not dark
+
             home: HomeScreen(title: 'Home'),
+
           );
         },
       ),
